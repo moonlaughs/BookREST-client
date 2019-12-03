d8bdfb7e901807d3929bf203fd0e1c5225c26052
Diiiya <33103806+Diiiya@users.noreply.github.com>
2019-12-03 19:48:29 +0200

    Book suggestions bar
    New functional component with useAsync and Edge support added to display a list for Suggested books

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
